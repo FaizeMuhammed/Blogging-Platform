deb2641cb172780e543d20f3cd156f57126f29b1
FaizeMuhammed <168710031+FaizeMuhammed@users.noreply.github.com>
2024-09-19 10:59:01 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express=require("express");
+const expressLayout=require("express-ejs-layouts");
+
+const connectDB=require('./server/config/db');
+
+const app=express();
+const PORT=3000 ||process.env.PORT;
+
+
+// connect to db
+connectDB();
+
+
+
+
+app.use(express.static("public"));
+
+
+// Template Engines
+app.use(expressLayout);     
+app.set("layout",'./layouts/main');
+app.set('view engine','ejs');
+
+app.use('/',require("./server/routes/main"))
+
+app.listen(PORT,()=>{
+    console.log(`server running on ${PORT} port`);
+})
---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require("mongoose");
+const connectDB= async()=>{
+    try{
+        mongoose.set('strictQuery',false);
+        const conn= await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`database connected to: ${conn.connection.host}`);
+    }
+    catch(error){
+        console.log(error);
+        
+    }
+}
+
+module.exports=connectDB;
